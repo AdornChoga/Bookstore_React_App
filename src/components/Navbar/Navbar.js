67dee1969fe6cc8bd5136d412ae1cf43da46900b
asanchy <sanchyclips@gmail.com>
2022-02-21 18:37:42 +0200

    Create nav-bar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+const Navbar = () => (
+  <header>
+    <nav>
+      <ul>
+        <li>BookStore CMS</li>
+        <li>BOOKS</li>
+        <li>CATEGORIES</li>
+        <li className="profile">profile</li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Navbar;
