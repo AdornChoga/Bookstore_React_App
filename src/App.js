22dac1587977044dcfb92de1814e1ceec527f560
asanchy <sanchyclips@gmail.com>
2022-02-21 19:45:31 +0200

    Create book component and add-new-book form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import Navbar from './components/Navbar/Navbar';
+import React from 'react';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Book from './components/book/Book';
+import AddBook from './components/addbook/AddBook';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <main>
+        <Book />
+        <Book />
+        <Book />
+        <AddBook />
+      </main>
+    </>
   );
 }
 
