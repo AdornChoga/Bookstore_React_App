57bc0af38030a9122a58581fb4bc897bf08da53e
asanchy <sanchyclips@gmail.com>
2022-02-25 16:47:32 +0200

    Add key props to all elements in book component

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -19,31 +19,31 @@ const Book = (props) => {
     dispatch(removeBook({ id }));
   };
   return (
-    <div className="books-container">
-      <ul>
-        <li id="category">{category}</li>
-        <li id="title">{title}</li>
-        <li id="author">{author}</li>
-        <li className="book-operations">
-          <p>Comments</p>
-          <button type="button" className="remove-book" onClick={handleRemove}>Remove</button>
-          <p>Edit</p>
+    <div className="books-container" key={Date.now()}>
+      <ul key={Date.now()}>
+        <li key={Date.now()} id="category">{category}</li>
+        <li key={Date.now()} id="title">{title}</li>
+        <li key={Date.now()} id="author">{author}</li>
+        <li key={Date.now()} className="book-operations">
+          <p key={Date.now()}>Comments</p>
+          <button key={Date.now()} type="button" className="remove-book" onClick={handleRemove}>Remove</button>
+          <p key={Date.now()}>Edit</p>
         </li>
       </ul>
-      <div className="progress-and-chapter">
-        <ul className="progress">
-          <li>Photo</li>
-          <li>
-            <span>{progress}</span>
+      <div className="progress-and-chapter" key={Date.now()}>
+        <ul className="progress" key={Date.now()}>
+          <li key={Date.now()}>Photo</li>
+          <li key={Date.now()}>
+            <span key={Date.now()}>{progress}</span>
             <br />
-            <span>Completed</span>
+            <span key={Date.now()}>Completed</span>
           </li>
         </ul>
         <ul>
-          <li>Current Chapter</li>
-          <li>{chapter}</li>
-          <li>
-            <button type="button" className="update-progress">Update Progress</button>
+          <li key={Date.now()}>Current Chapter</li>
+          <li key={Date.now()}>{chapter}</li>
+          <li key={Date.now()}>
+            <button key={Date.now()} type="button" className="update-progress">Update Progress</button>
           </li>
         </ul>
       </div>
